ccd990f0b1d423296c9086b128d80ef60afa58d6
Corvus <wyatta.hancock@gmail.com>
2020-07-29 14:15:06 -0600

    attempting to render and write the html, but not suceeding

--- Develop/app.js
+++ Develop/app.js
@@ -198,7 +198,9 @@ function emailValidate(email){
     }
 
 }
-
+function createHtml (){
+    fs.writeFile(outputPath, render(teamTotal), "utf-8")
+}
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
