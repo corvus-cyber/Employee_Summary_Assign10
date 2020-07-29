75ecd2fe09d22916b1f9c46cf70cb8d189091138
Corvus <wyatta.hancock@gmail.com>
2020-07-29 12:50:07 -0600

    beginning attempt at render.js

--- Develop/app.js
+++ Develop/app.js
@@ -9,7 +9,12 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-var seenID = {};
+
+//variable to place id's inside and verify to make sure they are unique
+const seenID = {};
+const teamTotal = [];
+
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
@@ -42,6 +47,7 @@ function addTeamMember (){
                 break;
             case "I do not want to add any more team members":
                 render(teamTotal);
+                createHtml();
                 break;
         }
     })
@@ -196,6 +202,7 @@ function emailValidate(email){
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
